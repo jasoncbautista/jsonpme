cba7dac7623fffafb188db2f00df1786d48d306a
Jason <bjason@gmail.com>
2014-09-21 22:01:53 -0700

    JSONP testing.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json, requests
 import logging
 
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
@@ -10,9 +10,18 @@ import pdb
 
 
 
-@app.route ("/jsonpme"):
-    pdb.set_trace()
-    return ""
+def generate_jsonp(callback_function_name, data):
+    data_string = "{'data': True}";
+    jsonped = "<script>" + callback_function_name + "(" + data_string  +  "); </script>"
+    return jsonped
+
+@app.route ("/jsonpme")
+def jsonpme():
+    callback_function = request.args.get("callback")
+    url = request.args.get("url")
+    return generate_jsonp(callback_function, "{}")
+
+
 @app.route("/helloworld")
 def render_success():
     sample_json   =  {"success": True, "data": ["one", "two", "three"]}
