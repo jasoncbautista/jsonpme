5cd9b052b4593ed8134df24c5d4bd6037c47a497
Jason <bjason@gmail.com>
2014-09-21 22:42:57 -0700

    Making a sub object, more complex.

--- test_server.py
+++ test_server.py
@@ -4,11 +4,16 @@ import logging
 from flask import Flask, jsonify
 app = Flask(__name__)
 
+@app.route("/json_api")
+def render_json():
+    sample_json   =  {"success": True, "data": ["one", "two", "three"]}
+    return jsonify(**sample_json) 
+
 
 
 @app.route("/helloworld")
-def render_success():
-    sample_json   =  {"success": True, "data": ["one", "two", "three"]}
+def render_helloworld():
+    sample_json   =  {"success": True, "data": ["one", "two", "three", {"mini_data": "okay"}]}
     return jsonify(**sample_json) 
 
 if __name__ == "__main__":
