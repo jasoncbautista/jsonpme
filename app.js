2cb190ee9d2047632a38fd7051809eb2c8fea0f9
Jason B <bjason@gmail.com>
2015-07-01 02:54:05 -0700

    Yoda speak!

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ var server = http.createServer(function(req, res){
     console.log(req.url);
     res.end("---");
 
-
-    console.log(url.parse(req.url, true).query);
+    var params = url.parse(req.url, true).query;
+    console.log(params);
+    console.log('url=', params['url']);
 
 
 });
@@ -21,6 +22,48 @@ server.listen(3000, function(){
 
 });
 
+
+
+// Learning how to make a simple Request with node:
+
+// Fake a curl: http://stackoverflow.com/questions/21408263/node-js-http-get-request-gets-a-totally-different-set-of-headers-from-a-curl-req
+//
+
+
+
+var host = "yoda.p.mashape.com"
+var path = "/yoda?sentence=You+will+learn+how+to+speak+like+me+someday.++Oh+wait."
+
+var headers = { 'X-Mashape-Key': 'g4nY5DLOwKmshEIgrtV3YPDWSAjxp1MW52TjsnSYxOwOo3b5gH'
+    , 'Accept': 'text/plain'
+    , accept: '*/*'
+}
+
+
+
+var options = {
+        host:   host
+    ,   port: 443
+    ,   path:    path
+    ,   headers: headers
+    ,   method : "GET"
+};
+
+
+var https = require("https");
+var req = https.get(options, function(res){
+    console.log("Got response: " + res.statusCode);
+    res.on("data", function(chunk) {
+        console.log("BODY: " + chunk);
+    });
+
+});
+
+
+
+req.on('error', function(e) {
+      console.log("Got error: " + e.message);
+});
 
 
 
