1a7b0e431e916ccc8d27d6db8e76cf89b0418459
Jason B <bjason@gmail.com>
2015-07-01 01:40:23 -0700

    Finally we get a lil parse of params

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var http = require("http");
+var url = require('url') ;
 
+var urlParser= require("fast-url-parser");
+urlParser.replace();
+
+//    var parsed =  urlParser("http://cool.com/?wassa=123231&url=xx", true);
 var server = http.createServer(function(req, res){
 
     console.log(req.url);
     res.end("---");
+
+
+    console.log(url.parse(req.url, true).query);
+
 
 });
 
